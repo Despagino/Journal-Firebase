78907fdbb922a09125fdb46b7231200df139eaf7
Gino Tasis <gtasis84@gmail.com>
2022-03-24 11:37:37 -0400

    Create read write functions using firebase

--- Journal+Firebase/Model Controller/EmployeeController.swift
+++ Journal+Firebase/Model Controller/EmployeeController.swift
@@ -6,19 +6,54 @@
 //
 
 import Foundation
-
+import Firebase
 
 struct EmployeeController {
 
+    
+    static let shared = EmployeeController()
+    let dbRes = Database.database().reference()
 
     //MARK: - Save
+        
+    func save(name: String, status: String) {
+        dbRes.childByAutoId().setValue(["name": "\(name)", "status": "\(status)"])
+    }
 
     //MARK: - read all (fetch all)
-
+    
+    func readAll() {
+        dbRes.observeSingleEvent(of: .value) { snapShot in
+            
+            guard let dictionary = snapShot.value as? [String: [String: Any]] else {
+                print("Error reading all items")
+                return
+            }
+            
+            print(dictionary.values.description)
+            
+            Array(dictionary.values).forEach {
+                let name = $0["name"] as? String ?? ""
+                let status = $0["status"] as? String ?? ""
+                print(name, status)
+                
+            }
+        }
+    }
+    
 
     //MARK: - read (fetch one)
-
+    
+    func readEmployee() {
+        dbRes.child("employee").observeSingleEvent(of: .value) { snapshot in
+            let employeeData = snapshot.value as? [String: Any]
+            print(employeeData ?? "No data was able to be read")
+        }
+    }
+    
 
     //MARK: - Update
-
+    
+    
+    
 }
